64d9d0e0d6a929b6ace3c8e57962c008118921c6
Tony Kanyamuka <tonykanyamuka@gmail.com>
2024-08-06 15:49:24 +0200

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Tony on 8/6/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
