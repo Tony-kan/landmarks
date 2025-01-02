68b6282756621b393020fb843b3f91a2714a792e
Tony Kanyamuka <tonykanyamuka@gmail.com>
2025-01-02 22:03:54 +0200

    implemented dynamic  data rendering of list,their landmark details and navigation from list item to details

---
+++ Landmarks/Views/LandmarkDetails.swift
@@ -0,0 +1,18 @@
+//
+//  LandmarkDetails.swift
+//  Landmarks
+//
+//  Created by Tony on 1/2/25.
+//
+
+import SwiftUI
+
+struct LandmarkDetails: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LandmarkDetails()
+}
